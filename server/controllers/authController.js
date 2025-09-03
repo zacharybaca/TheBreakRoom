9e97a48133000ca579c72b380df097b1fbc37c5f
Zachary Baca <zachary.baca@msn.com>
2025-09-03 09:15:14 -0500

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,22 +2,39 @@ import User from "../models/User.js";
 import generateToken from "../utils/generateToken.js";
 
 export const login = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+  try {
+    const { identifier, password } = req.body; 
+    // identifier = username OR email
 
-        const userExists = await User.findOne({ $or: [{ username }, { email }] });
+    // Search by username OR email, and explicitly include password
+    const user = await User.findOne({
+      $or: [{ email: identifier }, { username: identifier }],
+    }).select("+password");
 
-        if (!userExists) return res.status(404).json({ message: "Username or E-mail is invalid" });
-
-        if (!userExists.comparePassword(password)) return res.status(403).json({ message: "Wrong Username and/or Password" });
-
-        const token = generateToken(userExists._id, userExists.username);
-
-        return res.status(201).send({
-            user: { _id: userExists._id, username: userExists.username },
-            token,
-        });
-    } catch (err) {
-        res.status(500).json({ message: "Error logging in", error: err.message });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username/email or password" });
     }
+
+    // Compare password
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid username/email or password" });
+    }
+
+    // Success: return user info and token
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      isAdmin: user.isAdmin,
+      job: user.job,
+      bio: user.bio,
+      avatarUrl: user.avatarUrl,
+      token: generateToken(user._id, user.username),
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Error logging in", error: err.message });
+  }
 };
