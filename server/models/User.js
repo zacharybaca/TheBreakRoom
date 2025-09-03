506a11e9f13e5ee5110729d12ff31629a851c1bf
Zachary Baca <zachary.baca@msn.com>
2025-09-03 11:53:03 -0500

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,23 +4,29 @@ import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    username: { type: String, unique: true, sparse: true }, // now optional
-    email: { type: String, unique: true, sparse: true }, // now optional
+
+    username: { type: String, unique: true, sparse: true }, // optional, unique if present
+    email: { type: String, unique: true, sparse: true }, // optional, unique if present
+
     password: { type: String, required: true, select: false },
+
     role: {
       type: String,
       enum: ["user", "admin"],
       default: "user",
     },
+
     job: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Job",
       required: true,
     },
+
     isAdmin: {
       type: Boolean,
       default: false,
     },
+
     bio: { type: String },
     gender: {
       type: String,
@@ -32,65 +38,61 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Require at least one identifier (username or email)
+/**
+ * Require at least one identifier (username or email)
+ */
 userSchema.pre("validate", function (next) {
   if (!this.username && !this.email) {
-    this.invalidate(
-      "username",
-      "Either username or email is required."
-    );
-    this.invalidate(
-      "email",
-      "Either username or email is required."
-    );
+    this.invalidate("username", "Either username or email is required.");
+    this.invalidate("email", "Either username or email is required.");
   }
   next();
 });
 
-// Hash password + set avatar + sync isAdmin
+/**
+ * Pre-save hook: hash password, set default avatar, sync isAdmin
+ */
 userSchema.pre("save", async function (next) {
   try {
+    // hash password if modified
     if (this.isModified("password")) {
       this.password = await bcrypt.hash(this.password, 10);
     }
 
+    // set default avatar if none provided
     if (!this.avatarUrl) {
-      if (this.gender === "male") {
-        this.avatarUrl = "https://avatar.iran.liara.run/public/boy";
-      } else if (this.gender === "female") {
-        this.avatarUrl = "https://avatar.iran.liara.run/public/girl";
-      } else {
-        this.avatarUrl = "https://avatar.iran.liara.run/public";
+      switch (this.gender) {
+        case "male":
+          this.avatarUrl = "https://avatar.iran.liara.run/public/boy";
+          break;
+        case "female":
+          this.avatarUrl = "https://avatar.iran.liara.run/public/girl";
+          break;
+        default:
+          this.avatarUrl = "https://avatar.iran.liara.run/public";
       }
     }
 
+    // sync admin status
     this.isAdmin = this.role === "admin";
+
     next();
   } catch (err) {
     next(err);
   }
 });
 
-// Compare password method
-userSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
-
-// Reset password method
-userSchema.methods.resetPassword = async function (newPassword) {
-  this.password = await bcrypt.hash(newPassword, 10);
-  await this.save();
-};
-
-// Handle updates: hash password if updated + sync isAdmin with role
+/**
+ * Pre-update hook: hash password & sync isAdmin on updates
+ */
 userSchema.pre("findOneAndUpdate", async function (next) {
   const update = this.getUpdate();
 
-  if (update && update.password) {
+  if (update.password) {
     update.password = await bcrypt.hash(update.password, 10);
   }
 
-  if (update && update.role) {
+  if (update.role) {
     update.isAdmin = update.role === "admin";
   }
 
@@ -98,7 +100,21 @@ userSchema.pre("findOneAndUpdate", async function (next) {
   next();
 });
 
-// Transform response (remove sensitive fields)
+/**
+ * Instance methods
+ */
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.resetPassword = async function (newPassword) {
+  this.password = await bcrypt.hash(newPassword, 10);
+  await this.save();
+};
+
+/**
+ * Clean response
+ */
 function removeSensitive(doc, ret) {
   delete ret.password;
   delete ret.__v;
