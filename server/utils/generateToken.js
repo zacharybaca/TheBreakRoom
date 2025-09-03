0c1bb7e5532a1a06ba307e592b1956070acff444
Zachary Baca <zachary.baca@msn.com>
2025-09-03 09:36:21 -0500

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,7 +1,12 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (id, username) => {
-  return jwt.sign({ _id: id, username: username }, process.env.JWT_SECRET, { expiresIn: "30d" });
+const generateToken = (id, identifier) => {
+  // identifier can be username OR email
+  return jwt.sign(
+    { _id: id, identifier }, 
+    process.env.JWT_SECRET, 
+    { expiresIn: "30d" }
+  );
 };
 
 export default generateToken;
