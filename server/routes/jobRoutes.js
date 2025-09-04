5c6664175de92ceb0851c5b16cb3b78cbcc45069
root <root@Zachs-PC.>
2025-09-04 06:08:04 -0500

    updated descriptions for job routes

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -11,17 +11,47 @@ import { protect, requireAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+/**
+ * @route POST /api/jobs
+ * @desc Create a new job
+ * @access Private (Admin only)
+ */
+
 // Create job (admin only)
 router.post("/", protect, requireAdmin, createJob);
+
+/**
+ * @route GET /api/jobs
+ * @desc Get all jobs
+ * @access Public
+ */
 
 // Get all jobs
 router.get("/", getJobs);
 
+/**
+ * @route GET /api/jobs/:id
+ * @desc Get job by Id
+ * @access Public
+ */
+
 // Get job by Id
 router.get("/:id", getJobById);
 
+/**
+ * @route PUT /api/jobs/:id
+ * @desc Update job by Id
+ * @access Private (Admin only)
+ */
+
 // Update job
 router.put("/:id", protect, requireAdmin, updateJob);
+
+/**
+ * @route DELETE /api/jobs/:id
+ * @desc Delete job by Id
+ * @access Private (Admin only)
+ */
 
 // Delete job
 router.delete("/:id", protect, requireAdmin, deleteJob);
