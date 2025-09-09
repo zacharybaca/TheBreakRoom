95f0285c9b86dc94762d67c7a8717bc23d2c545c
Zachary Baca <zachary.baca@msn.com>
2025-09-09 13:27:16 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from "./components/NavBar/NavBar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
 
   return (
     <div id="main-app-container">
       <NavBar />
+      <Footer />
     </div>
   )
 }
