3b54f50e20fc9454a71551d28db806dd9841db81
Zachary Baca <zachary.baca@msn.com>
2025-09-09 10:48:29 -0500

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,6 +4,17 @@ const NavBar = () => {
 
   return (
     <nav>
+      <div id="menu-icon-container">
+        <h1>Menu Icon Placholder</h1>
+      </div>
+      <div id="app-icon-container">
+        <img src="/assets/Nine2Five-logo.png" alt="app logo" />
+        <h1>The platform for the people who keep things moving.</h1>
+        <p>From the breakroom to the world — we speak.</p>
+      </div>
+      <div id="notification-icon-container">
+        <h1>Notification Icon Placeholder</h1>
+      </div>
     </nav>
   )
 
