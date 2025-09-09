b92075a90ad7454ec0ee3e7a197e1df1d71096e9
Zachary Baca <zachary.baca@msn.com>
2025-09-09 10:32:52 -0500

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,17 +1,35 @@
 // routes/authRoutes.js
 import express from "express";
-import { login, register } from "../controllers/authController.js";
+import { login, logout, register, refreshAccessToken } from "../controllers/authController.js";
 
 const router = express.Router();
 
-// @desc    Register a new user (self-registration)
-// @route   POST /api/auth/register
-// @access  Public
+/**
+ * @route   POST /api/auth/register
+ * @desc    Register a new user (self-registration)
+ * @access  Public
+ */
 router.post("/register", register);
 
-// @desc    Authenticate user & get token
-// @route   POST /api/auth/login
-// @access  Public
+/**
+ * @route   POST /api/auth/login
+ * @desc    Authenticate user & get token
+ * @access  Public
+ */
 router.post("/login", login);
+
+/**
+ * @route POST /api/auth/logout
+ * @desc Logout a current "logged-in" user
+ * @access Public
+ */
+router.post("/logout", logout);
+
+/**
+ * @route POST /api/auth/refresh
+ * @desc Refreshes user's token
+ * @access Private
+ */
+router.post("/refresh", refreshAccessToken);
 
 export default router;
