e91a542916fea3a8cf162c55ff56557a4bc0d6ea
Zachary Baca <zachary.baca@msn.com>
2025-09-10 13:18:37 -0500

    Create SlideOutMenu.jsx

---
+++ client/src/components/SlideOutMenu/SlideOutMenu.jsx
@@ -0,0 +1,12 @@
+import './slide-out-menu.css';
+
+const SlideOutMenu = () => {
+
+    return (
+        <div>
+            <h1>Slide OUt Menu Placholder</h1>
+        </div>
+    )
+}
+
+export default SlideOutMenu;
