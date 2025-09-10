145c4b2bc1f06c5745251a21a95ca3b3d6c1714f
Zachary Baca <zachary.baca@msn.com>
2025-09-10 08:32:00 -0500

    Update ToggleProvider.jsx

--- client/src/contexts/Toggle/ToggleProvider.jsx
+++ client/src/contexts/Toggle/ToggleProvider.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import { ToggleContext } from './ToggleContext.jsx';
 
 export const ToggleProvider = ({ children }) => {
+    const [notificationsOn, setNotificationsOn] = useState(true);
 
     return (
-        <ToggleContext.Provider>
+        <ToggleContext.Provider value={{
+            notificationsOn
+        }}>
             {children}
         </ToggleContext.Provider>
     )
