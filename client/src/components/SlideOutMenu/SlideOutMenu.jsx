ec6d54090ddcd25bfb18535bfd3f04dfa51a94d2
Zachary Baca <zachary.baca@msn.com>
2025-09-11 09:07:13 -0500

    Update SlideOutMenu.jsx

--- client/src/components/SlideOutMenu/SlideOutMenu.jsx
+++ client/src/components/SlideOutMenu/SlideOutMenu.jsx
@@ -1,6 +1,8 @@
 import './slide-out-menu.css';
+import { useAuth } from "../../hooks/useAuth";
 
 const SlideOutMenu = ({ isOpen, onClose }) => {
+    const { isAuthenticated } = useAuth();
 
     return (
         <>
@@ -10,13 +12,12 @@ const SlideOutMenu = ({ isOpen, onClose }) => {
             {/* Slideout Menu */}
             <div className={`slideout ${isOpen ? "open" : ""}`}>
                 <button className="close-btn" onClick={onClose}>×</button>
-                  <button type="button"><img src="/assets/log-on-icon.png" alt="log in logo" /></button>
+                {isAuthenticated ? <button type="button"><img src="/assets/log-off-icon.png" alt=" log off icon" /></button> : <button type="button"><img src="/assets/log-on-icon.png" alt="log in logo" /></button>}
                 <ul>
                     <li><a href="/profile"><img src="/assets/profile-icon.png" className="menu-image-class" alt="profile icon" />My Profile</a></li>
                     <li><a href="/feed"><img src="/assets/news-feed-icon.png" className="menu-image-class" alt="news feed icon" /> Community Feed</a></li>
                     <li><a href="/notifications">Notifications</a></li>
                     <li><a href="/settings"><img src="/assets/settings-icon.png" className="menu-image-class" alt="settings icon" />Settings</a></li>
-                    <li><a href="/logout">Logout</a></li>
                 </ul>
             </div>
         </>
