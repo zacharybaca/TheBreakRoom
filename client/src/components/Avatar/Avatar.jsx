5e82cefe502fbed4fd59f4cb0466d550c8a151b0
Zachary Baca <zachary.baca@msn.com>
2025-09-11 11:35:26 -0500

    Update Avatar.jsx

--- client/src/components/Avatar/Avatar.jsx
+++ client/src/components/Avatar/Avatar.jsx
@@ -1,12 +1,14 @@
 import './avatar.css';
+import { useAuth } from "../../hooks/useAuth";
 
 const Avatar = () => {
+    const { user } = useAuth();
 
     return (
         <div>
-            Avatar Placeholder
+            {user && <img src={user.avatarUrl} alt="User Avatar" />}
         </div>
-    )
-}
+    );
+};
 
 export default Avatar;
