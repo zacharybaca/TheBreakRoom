06581117415d35a2b4b51a66a3a976f70cf878b4
Zachary Baca <zachary.baca@msn.com>
2025-09-19 09:30:54 -0500

    Update ModalProvider.jsx

--- client/src/contexts/Modal/ModalProvider.jsx
+++ client/src/contexts/Modal/ModalProvider.jsx
@@ -1,1 +1,21 @@
+import { useState } from 'react';
+import { ModalContext } from './ModalContext.jsx';
 
+export const ModalProvider = ({ children }) => {
+    const [step, setStep] = useState(1);
+    const [direction, setDirection] = useState(0); // 1 = next, -1 = back
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <ModalContext.Provider value={{
+            step,
+            setStep,
+            direction,
+            setDirection,
+            isOpen,
+            setIsOpen
+        }}>
+            {children}
+        </ModalContext.Provider>
+    )
+}
