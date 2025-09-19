c8d2468260a1b508fd96d669c0f286985b6b3410
Zachary Baca <zachary.baca@msn.com>
2025-09-19 09:25:21 -0500

    Update AppProvider.jsx

--- client/src/contexts/AppProvider.jsx
+++ client/src/contexts/AppProvider.jsx
@@ -3,17 +3,20 @@ import { FetcherProvider } from './Fetcher/FetcherProvider';
 import { AuthProvider } from './Auth/AuthProvider';
 import { ToggleProvider } from './Toggle/ToggleProvider';
 import { ConfirmationProvider } from './Confirmation/ConfirmationProvider';
+import { ModalProvider } from './Modal/ModalProvider';
 
 export const AppProvider = ({ children }) => {
   return (
     <Router>
-      <ConfirmationProvider>
+      <ModalProvider>
+        <ConfirmationProvider>
         <ToggleProvider>
           <AuthProvider>
             <FetcherProvider>{children}</FetcherProvider>
           </AuthProvider>
         </ToggleProvider>
       </ConfirmationProvider>
+      </ModalProvider>
     </Router>
   );
 };
