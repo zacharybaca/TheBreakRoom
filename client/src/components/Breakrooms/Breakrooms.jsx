0c6212170746c378391021776752d5e6086965fb
Zachary Baca <zachary.baca@msn.com>
2025-09-24 13:03:24 -0500

    Create Breakrooms.jsx

---
+++ client/src/components/Breakrooms/Breakrooms.jsx
@@ -0,0 +1,12 @@
+import './breakrooms.css';
+
+const Breakrooms = () => {
+
+    return (
+        <div>
+            <h1>Breakroom Placeholder</h1>
+        </div>
+    )
+}
+
+export default Breakrooms;
