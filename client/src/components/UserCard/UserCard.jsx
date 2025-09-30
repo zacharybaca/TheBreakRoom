89911c07639a705508ce7f2e8d7faa6f8b2043e0
Zachary Baca <zachary.baca@msn.com>
2025-09-30 07:40:14 -0500

    Create UserCard.jsx

---
+++ client/src/components/UserCard/UserCard.jsx
@@ -0,0 +1,12 @@
+import './user-card.css';
+
+const UserCard = () => {
+
+    return (
+        <div id="user-card">
+            <h1>User Card Placeholder</h1>
+        </div>
+    )
+}
+
+export default UserCard;
