d8e85217bd2f7fb049112f1b464d76c6a9a8a1d3
Zachary Baca <zachary.baca@msn.com>
2025-09-30 07:38:04 -0500

    Create Users.jsx

---
+++ client/src/components/Users/Users.jsx
@@ -0,0 +1,14 @@
+import './users.css';
+
+const Users = () => {
+
+    return (
+        <div id="users-container">
+            <div id="user-card-container">
+                <h1>User Card Placeholder</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Users;
