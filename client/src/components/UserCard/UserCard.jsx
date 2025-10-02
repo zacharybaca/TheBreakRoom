7e397c256718c383a8032f1fce1ef69d70d92c01
Zachary Baca <zachary.baca@msn.com>
2025-10-02 08:59:56 -0500

    Update UserCard.jsx

--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -1,12 +1,14 @@
 import './user-card.css';
 
-const UserCard = () => {
-
-    return (
-        <div id="user-card">
-            <h1>User Card Placeholder</h1>
-        </div>
-    )
-}
+const UserCard = (props) => {
+  return (
+    <div id="user-card">
+      <img src={props.image} alt="user" />
+      <h3>Name: {props.firstName} {props.lastName}</h3>
+      <p>Username: {props.username}</p>
+      <p>Email: {props.email}</p>
+    </div>
+  );
+};
 
 export default UserCard;
