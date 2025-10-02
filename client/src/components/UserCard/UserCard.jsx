b12a4e4b6781a279cd6bafa4acb07ed031baf536
Zachary Baca <zachary.baca@msn.com>
2025-10-02 14:11:01 -0500

    Update UserCard.jsx

--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -4,7 +4,7 @@ const UserCard = (props) => {
   return (
     <div id="user-card">
       <img src={props.image} alt="user" />
-      <h3 className="info-text name-font">Name: {props.firstName} {props.lastName}</h3>
+      <h3 className="info-text name-font-alt-style">Name: {props.firstName} {props.lastName}</h3>
       <p className="info-text">Username: {props.username}</p>
       <p className="info-text">Email: {props.email}</p>
     </div>
