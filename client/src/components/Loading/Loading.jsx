6d1976a0bd4fd80792ead45936ed8c79d6c89ba9
Zachary Baca <zachary.baca@msn.com>
2025-10-09 13:49:51 -0500

    Update Loading.jsx

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
 
     return (
         <div id="loading-container">
-            <h1>Loading Placeholder</h1>
+            <h1>Getting Your Personal Space Ready.....</h1>
             <img src="/assets/Loading_Your_Platform.gif" alt="loading gif" />
         </div>
     )
