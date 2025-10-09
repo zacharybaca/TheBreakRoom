d529c16ada9be611c9bea05516cc760682601926
Zachary Baca <zachary.baca@msn.com>
2025-10-09 12:28:28 -0500

    Create Loading.jsx

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import './loading.css';
+
+const Loading = () => {
+
+    return (
+        <div id="loading-container">
+            <h1>Loading Placeholder</h1>
+        </div>
+    )
+}
+
+export default Loading;
