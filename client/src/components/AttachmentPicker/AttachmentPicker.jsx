78ba7c0a474f2c17442d3a0528fe2062ca632a3c
Zachary Baca <zachary@Zachs-PC.>
2025-10-11 21:26:29 -0500

    made adjustments to position of user cards and added picker component

---
+++ client/src/components/AttachmentPicker/AttachmentPicker.jsx
@@ -0,0 +1,12 @@
+import './attachment-picker.css';
+
+const AttachmentPicker = () => {
+
+    return (
+        <div id="picker-container">
+            <h1>Attachment Picker Placeholder</h1>
+        </div>
+    )
+}
+
+export default AttachmentPicker;
