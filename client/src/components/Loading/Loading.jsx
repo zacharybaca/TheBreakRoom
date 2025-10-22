c0fbff9ed3bc064ae0c49bf8bd2578bd320ca7fc
Zachary Baca <zachary.baca@msn.com>
2025-10-22 13:29:13 -0500

    Refactor Loading component to remove loadingMessage
    Removed loadingMessage prop and adjusted component.

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,10 +1,9 @@
 import './loading.css';
 
-const Loading = ({ loadingMessage }) => {
+const Loading = () => {
 
     return (
         <div id="loading-container">
-            <h1>{loadingMessage}</h1>
             <img src="/assets/coffee-time-loading.gif" alt="loading gif" />
         </div>
     )
