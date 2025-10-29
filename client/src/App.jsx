4b78bce3baa02eea168f478e23dce6b9ce4f105f
Zachary Baca <zachary.baca@msn.com>
2025-10-29 12:27:15 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,9 +19,9 @@ function App() {
   return (
     <div id="main-app-container">
       <NavBar />
-      <div className="jumbotron-container">
+      {!isAuthenticated ? <div className="jumbotron-container">
         <img src="/assets/app_homepage_logo.png" id="jumbotron-img" alt="homepage jumbotron" />
-      </div>
+      </div> : ""}
       <main id="content">
         <Routes>
           <Route path="/" element={<Login />} />
