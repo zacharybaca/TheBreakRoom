b6590de33e5b4b79667609023aca94d0b981200a
Zachary Baca <zachary.baca@msn.com>
2025-11-05 14:39:47 -0600

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import argon2 from "argon2";
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
 
-    username: { type: String, unique: true, sparse: true }, // optional, unique if present
-    email: { type: String, unique: true, sparse: true }, // optional, unique if present
+    username: { type: String, unique: true, sparse: true },
+    email: { type: String, unique: true, sparse: true },
 
     password: { type: String, required: true, select: false },
 
@@ -60,12 +60,17 @@ userSchema.pre("validate", function (next) {
  */
 userSchema.pre("save", async function (next) {
   try {
-    // hash password if modified
     if (this.isModified("password")) {
-      this.password = await bcrypt.hash(this.password, 10);
+      // Use Argon2id for new or updated passwords
+      this.password = await argon2.hash(this.password, {
+        type: argon2.argon2id,
+        memoryCost: 2 ** 16, // 64 MB
+        timeCost: 3,
+        parallelism: 1,
+      });
     }
 
-    // set default avatar if none provided
+    // Default avatar logic
     if (!this.avatarUrl) {
       switch (this.gender) {
         case "male":
@@ -79,9 +84,7 @@ userSchema.pre("save", async function (next) {
       }
     }
 
-    // sync admin status
     this.isAdmin = this.role === "admin";
-
     next();
   } catch (err) {
     next(err);
@@ -95,7 +98,12 @@ userSchema.pre("findOneAndUpdate", async function (next) {
   const update = this.getUpdate();
 
   if (update.password) {
-    update.password = await bcrypt.hash(update.password, 10);
+    update.password = await argon2.hash(update.password, {
+      type: argon2.argon2id,
+      memoryCost: 2 ** 16,
+      timeCost: 3,
+      parallelism: 1,
+    });
   }
 
   if (update.role) {
@@ -109,12 +117,22 @@ userSchema.pre("findOneAndUpdate", async function (next) {
 /**
  * Instance methods
  */
-userSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  // Supports both Argon2 and old bcrypt hashes
+  if (this.password.startsWith("$2")) {
+    const bcrypt = await import("bcrypt");
+    return bcrypt.compare(candidatePassword, this.password);
+  }
+  return argon2.verify(this.password, candidatePassword);
 };
 
 userSchema.methods.resetPassword = async function (newPassword) {
-  this.password = await bcrypt.hash(newPassword, 10);
+  this.password = await argon2.hash(newPassword, {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 3,
+    parallelism: 1,
+  });
   await this.save();
 };
 
