862e7ef7e12b041936c05b710f158265722dff4c
Zachary Baca <zachary.baca@msn.com>
2025-11-06 09:50:07 -0600

    Add devTestMailer for creating test email transport

---
+++ server/utils/mail/devTestMailer.js
@@ -0,0 +1,17 @@
+import nodemailer from 'nodemailer';
+
+export async function createTestTransporter() {
+  const testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: testAccount.smtp.host,
+    port: testAccount.smtp.port,
+    secure: testAccount.smtp.secure,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  });
+
+  return { transporter, previewUrl: nodemailer.getTestMessageUrl };
+}
