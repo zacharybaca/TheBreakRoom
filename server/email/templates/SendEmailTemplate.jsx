72b4410ce26a91d8ec7663c26ba600907eed095d
Zachary Baca <zachary.baca@msn.com>
2025-11-11 13:43:18 -0600

    Enhance SendEmailTemplate with layout components
    Added new layout components and a container for better structure.

--- server/email/templates/SendEmailTemplate.jsx
+++ server/email/templates/SendEmailTemplate.jsx
@@ -1,5 +1,5 @@
 import './send-email-template.css';
-import { Html, Button } from "@react-email/components";
+import { Html, Button, Section, Row, Column, Container, Img, Link, Text } from "@react-email/components";
 
 
 const SendEmailTemplate = (props) => {
@@ -7,6 +7,53 @@ const SendEmailTemplate = (props) => {
 
     return (
         <Html lang="en">
+            <Section className="my-[40px] px-[32px] py-[40px]">
+                <Row>
+                    <Column align="center">
+                    <Img
+                        alt="React Email logo"
+                        height="42"
+                        src="https://react.email/static/logo-without-background.png"
+                    />
+                    </Column>
+                </Row>
+                <Row className="mt-[40px]">
+                    <Column align="center">
+                    <table>
+                        <tr>
+                        <td className="px-[8px]">
+                            <Link className="text-gray-600 [text-decoration:none]" href="#">
+                            About
+                            </Link>
+                        </td>
+                        <td className="px-[8px]">
+                            <Link className="text-gray-600 [text-decoration:none]" href="#">
+                            Blog
+                            </Link>
+                        </td>
+                        <td className="px-[8px]">
+                            <Link className="text-gray-600 [text-decoration:none]" href="#">
+                            Company
+                            </Link>
+                        </td>
+                        <td className="px-[8px]">
+                            <Link className="text-gray-600 [text-decoration:none]" href="#">
+                            Features
+                            </Link>
+                        </td>
+                        </tr>
+                    </table>
+                    </Column>
+                </Row>
+            </Section>
+
+            <Container className="bg-gray-400">
+                <Text className="px-[12px] text-white">
+                    Hello, I am a container. I keep content centered and maintain it to a
+                    maximum width while still taking up as much space as possible!
+                </Text>
+            </Container>
+
             <Button href={url}>Click me</Button>
         </Html>
     )
