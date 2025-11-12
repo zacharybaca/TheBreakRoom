8e301a98bc74b5f080085a88fbc3df3e3b9a299d
Zachary Baca <zachary.baca@msn.com>
2025-11-12 14:57:49 -0600

    Update SendEmailTemplate.jsx

--- server/email/templates/SendEmailTemplate.jsx
+++ server/email/templates/SendEmailTemplate.jsx
@@ -48,6 +48,45 @@ const SendEmailTemplate = (props) => {
             </Section>
 
             <Container className="bg-gray-400">
+                <Row>
+                    <Column align="center">
+                        <Img
+                        src="https://github.com/luxonauta.png?size=100"
+                        alt="Lucas de França"
+                        width="30"
+                        height="30"
+                        className="inline-block w-[30px] h-[30px] rounded-full"
+                        />
+                    </Column>
+                    <Column align="center">
+                        <Img
+                        src="https://github.com/luxonauta.png?size=100"
+                        alt="Lucas de França"
+                        width="42"
+                        height="42"
+                        className="inline-block w-[42px] h-[42px] rounded-full"
+                        />
+                    </Column>
+                    <Column align="center">
+                        <Img
+                        src="https://github.com/luxonauta.png?size=100"
+                        alt="Lucas de França"
+                        width="54"
+                        height="54"
+                        className="inline-block w-[54px] h-[54px] rounded-full"
+                        />
+                    </Column>
+                    <Column align="center">
+                        <Img
+                        src="https://github.com/luxonauta.png?size=100"
+                        alt="Lucas de França"
+                        width="66"
+                        height="66"
+                        className="inline-block w-[66px] h-[66px] rounded-full"
+                        />
+                    </Column>
+                </Row>
+                
                 <Text className="px-[12px] text-white">
                     Hello, I am a container. I keep content centered and maintain it to a
                     maximum width while still taking up as much space as possible!
