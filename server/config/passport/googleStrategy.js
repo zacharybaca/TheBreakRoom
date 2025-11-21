b810b008aaeafa0fbe95a7eb56634a4bdf2f2c05
Zachary Baca <zachary.baca@msn.com>
2025-11-21 07:46:16 -0600

    Update googleStrategy.js

--- server/config/passport/googleStrategy.js
+++ server/config/passport/googleStrategy.js
@@ -1,1 +1,34 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User from '../models/User.js';
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.SERVER_URL}/auth/google/callback`
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const { email, name } = extractGoogleProfile(profile);
+
+      let user = await User.findOne({ email });
+      if (!user) {
+        user = await User.create({
+          email,
+          username: name,
+          provider: 'google'
+        });
+      }
+
+      return done(null, user);
+    }
+  )
+);
+
+const extractGoogleProfile = (profile) => ({
+  email: profile.emails?.[0]?.value,
+  name: profile.displayName
+});
+
+export default passport;
