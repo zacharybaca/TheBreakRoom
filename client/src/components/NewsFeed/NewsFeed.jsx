7f6389a46cbe1d64179feecedf4ba565acb06156
Zachary Baca <zachary.baca@msn.com>
2025-12-01 09:31:20 -0600

    Update NewsFeed.jsx

--- client/src/components/NewsFeed/NewsFeed.jsx
+++ client/src/components/NewsFeed/NewsFeed.jsx
@@ -1,13 +1,27 @@
 import './news-feed.css';
 
-
 const NewsFeed = () => {
+  return (
+    <div id="news-feed">
+      <header className="nf-header">
+        <h1>News Feed</h1>
+      </header>
 
-    return (
-        <div id="news-feed">
-            <h1>News Feed Component</h1>
-        </div>
-    )
-}
+      <section className="nf-composer">
+        <textarea
+          className="nf-input"
+          placeholder="Share an update..."
+        />
+        <button className="nf-submit">Post</button>
+      </section>
+
+      <section className="nf-list">
+        <article className="nf-item">Example post 1</article>
+        <article className="nf-item">Example post 2</article>
+        <article className="nf-item">Example post 3</article>
+      </section>
+    </div>
+  );
+};
 
 export default NewsFeed;
