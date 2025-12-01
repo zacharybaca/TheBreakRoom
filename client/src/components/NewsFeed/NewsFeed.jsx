34aeda249443999d03821c575691acbe86baa935
Zachary Baca <zachary.baca@msn.com>
2025-12-01 07:42:10 -0600

    Create NewsFeed.jsx

---
+++ client/src/components/NewsFeed/NewsFeed.jsx
@@ -0,0 +1,13 @@
+import './news-feed.css';
+
+
+const NewsFeed = () => {
+
+    return (
+        <div id="news-feed">
+            <h1>News Feed Component</h1>
+        </div>
+    )
+}
+
+export default NewsFeed;
