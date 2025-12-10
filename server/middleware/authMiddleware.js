1d5b383dd9cf2febd64c5ca36b089a3c31685f8c
Zachary Baca <zachary.baca@msn.com>
2025-12-10 15:12:52 -0600

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -10,12 +10,17 @@ export const protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-      // We select '-password' so we don't accidentally leak hashes
       req.user = await User.findById(decoded.id).select("-password");
 
-      if (!req.user) {
-        return res.status(401).json({ message: "User not found" });
+      if (!req.user) return res.status(401).json({ message: "User not found" });
+
+      // --- NEW: THE BAN CHECK ---
+      if (req.user.isBanned) {
+        return res.status(403).json({
+          message: "Your account has been suspended. Please contact support."
+        });
       }
+      // --------------------------
 
       next();
     } catch (err) {
