6feea621aef0fe488bf51bf95293d233bd39d1f0
Zachary Baca <zachary.baca@msn.com>
2025-12-12 12:48:15 -0600

    Implement user serialization and deserialization
    Added user serialization and deserialization methods for passport authentication.

--- server/config/passport/index.js
+++ server/config/passport/index.js
@@ -1,2 +1,21 @@
+import passport from "passport";
+import User from "../../models/User.js"; // Import your User model
 import "./googleStrategy.js";
 import "./appleStrategy.js";
+
+// 1. Serialize: Save just the User ID to the session cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// 2. Deserialize: Take the ID from the cookie and find the full User object
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+export default passport;
