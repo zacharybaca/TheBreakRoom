6b97a9de78c942aeb274a7540c1116d7de0f37e7
Zachary Baca <zachary.baca@msn.com>
2026-01-13 08:29:56 -0600

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -53,6 +53,17 @@ const userSchema = new mongoose.Schema(
     banReason: {
       type: String,
     },
+    // 1. Add this to track activity
+  lastActive: {
+    type: Date,
+    default: Date.now
+  },
+
+  // 2. Add this to track status
+  isActive: {
+    type: Boolean,
+    default: true
+  },
   },
   { timestamps: true },
 );
