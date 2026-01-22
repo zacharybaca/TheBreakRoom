648c3e44b16da8cc1b3d24776a33affb9ee2f1e1
Zachary Baca <zachary.baca@msn.com>
2026-01-22 09:50:09 -0600

    Create ErrorRouteWrapper.jsx

---
+++ client/src/components/ErrorRouteWrapper/ErrorRouteWrapper.jsx
@@ -0,0 +1,19 @@
+import ErrorModal from "../ErrorModal/ErrorModal"
+import { useLocation, useNavigate } from 'react-router-dom'
+
+const ErrorRouteWrapper = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const { message, icon } = location.state || {};
+
+    return (
+        <ErrorModal
+            errorStatement={message || 'Something went wrong!'}
+            errorIcon={icon || '/assets/error.png'}
+            onClose={() => navigate('/')}
+        />
+    )
+}
+
+export default ErrorRouteWrapper;
