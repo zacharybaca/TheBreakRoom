30921e7b1ef04f51439d56bb96eb1121ccca29ae
Zachary Baca <zachary.baca@msn.com>
2026-01-22 13:46:00 -0600

    Update useUsers.js

---
+++ client/src/hooks/useUsers.js
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { UsersContext } from '../contexts/Users/UsersContext';
+
+export const useUsers = () => {
+    const context = useContext(UsersContext);
+    if (!context) {
+        throw new Error('useUsers must be used within a UsersProvider');
+    }
+    return context;
+};
