ad4d493b119d418dc35545d3882b7c429348f86d
Zachary Baca <zachary.baca@msn.com>
2026-01-27 11:24:30 -0600

    Update reportRoutes.js

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -2,8 +2,6 @@ import express from "express";
 import { protect, requireAdmin } from "../middleware/authMiddleware.js";
 import {
   createReport,
-  getReports,
-  resolveReport,
 } from "../controllers/reportController.js";
 
 const router = express.Router();
@@ -13,14 +11,8 @@ const router = express.Router();
 // @access  Private (Any logged in user)
 router.post("/", protect, createReport);
 
-// @route   GET /api/reports
-// @desc    Get all pending reports
-// @access  Admin Only
-router.get("/", protect, requireAdmin, getReports);
 
-// @route   PUT /api/reports/:reportId/resolve
-// @desc    Resolve a report (Dismiss or Delete Content)
-// @access  Admin Only
-router.put("/:reportId/resolve", protect, requireAdmin, resolveReport);
+
+
 
 export default router;
